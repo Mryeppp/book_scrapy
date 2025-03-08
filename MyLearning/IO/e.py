40222022d0fb55f5956b93467be183b0f5046e59
root <root@DESKTOP-57V187F>
2025-03-08 14:56:51 +0800

    添加f.py

--- MyLearning/IO/e.py
+++ MyLearning/IO/e.py
@@ -2,6 +2,25 @@
 # os模块的fork方法 仅支持unix\linux
 # multiprocessing模块 支持跨平台
 
+
+# fork 方法是调用一次返回两次，fork方法的原理是从当前进程中复制一个一模一样的进程
+# 所以会在父子进程都产生返回值，子进程返回0 ， 父进程返回子进程的ID
+
+# import os模块的fork方法
+'''
+import os
+if __name__ =='__main__' :
+    print('cur process (%s) start' %(os.getpid()))
+    pid = os.fork()
+    print(pid)
+    if pid < 0 :
+        print("error fork")
+    elif pid == 0:
+        print('i am a child process (%s) and my parent process is (%s)' %(os.getpid(),os.getppid()))
+        # getppid()返回当前进程的父进程ID,getpid()返回当前进程ID
+    else:
+        print('I (%s) create a process (%s)' %(os.getpid() , pid))
+'''
 # multiprocessing创建多进程
 import os
 from multiprocessing import Process
---
+++ MyLearning/IO/f.py
@@ -0,0 +1,37 @@
+# 线程池 pool
+# 当子线程较多的时候 无法像e.py展示的方法一样一个一个手动创建
+# multiprocessing 提供了 Pool类来代表线程池对象
+# 下面的示例中线程池容量为3。但是以此添加了五个任务，但是线程池容量决定了每次最多运行三个进程
+# 只有当一个任务结束了，新的任务才会进入线程池
+# 而且新任务使用的进程依然是原来的进程，这一点看每个任务的pid即可
+from multiprocessing import Pool 
+import os, time, random
+
+def run_task(name):
+    print ("process (%s) [pid = (%s)] is runing ..." %(name , os.getpid()))
+    time.sleep(random.random() * 3)
+    print('Task %s end' %(name))
+if __name__ == '__main__' :
+    print("cur process %s" %(os.getpid()))
+    p = Pool(processes = 3)
+    for i in range(5):
+        p.apply_async(run_task , args = (i, ))
+    print("waiting for all subprocesses done")
+    p.close()
+    p.join()
+    print("all subprocesses done")
+'''
+cur process 1961
+waiting for all subprocesses done
+process (0) [pid = (1962)] is runing ...
+process (1) [pid = (1963)] is runing ...
+process (2) [pid = (1964)] is runing ...
+Task 0 end
+process (3) [pid = (1962)] is runing ...
+Task 3 end
+process (4) [pid = (1962)] is runing ...
+Task 2 end
+Task 1 end
+Task 4 end
+all subprocesses done
+'''
